a40a4ebcc936d4282d4ce6fa83c4499fd2577cfa
Jakov <jakovdisic8@gmail.com>
2023-02-07 16:12:12 +0100

    Update js.js
    data for rendering in dom

---
+++ JavaScript/js.js
@@ -0,0 +1,85 @@
+var data = {
+  info: {
+    name: "Jakov",
+    surname: "Disic",
+    adress: {
+      country: "Serbia",
+      city: "Belgrade",
+      street: "Bata Lakina",
+      number: 1,
+    },
+    phone: "0692858301",
+    img: "",
+  },
+  education: [
+    {
+      title: "Union Nikola Tesla-poslovni i pravni fakultet",
+      desc: " Lorem ipsum dolor sit amet, consectetur adipisicing elit Consequatur consectetur, excepturi veniam necessitatibus adtempora quas alias error, dolore iusto dicta! Quasi assumenda  ipsum mollitia cumque ut doloremque, repudiandae accusantium?",
+      time: {
+        from: {
+          year: 0,
+        },
+        to: {
+          year: 0,
+        },
+      },
+    },
+    {
+      title: " Code by Comtrade",
+      desc: " Lorem ipsum dolor sit amet, consectetur adipisicing elit Consequatur consectetur, excepturi veniam necessitatibus adtempora quas alias error, dolore iusto dicta! Quasi assumenda  ipsum mollitia cumque ut doloremque, repudiandae accusantium?",
+      time: {
+        from: {
+          day: 0,
+          month: 0,
+          year: 0,
+        },
+      },
+    },
+  ],
+  projects: [
+    {
+      title: "DnD map Creator",
+      desc: " Lorem ipsum dolor sit amet, consectetur adipisicing elit Consequatur consectetur, excepturi veniam necessitatibus adtempora quas alias error, dolore iusto dicta! Quasi assumenda  ipsum mollitia cumque ut doloremque, repudiandae accusantium?",
+      time: {
+        year: 2009,
+        month: 6,
+      },
+    },
+    {
+      title: "DnD combat table",
+      desc: " Lorem ipsum dolor sit amet, consectetur adipisicing elit Consequatur consectetur, excepturi veniam necessitatibus adtempora quas alias error, dolore iusto dicta! Quasi assumenda  ipsum mollitia cumque ut doloremque, repudiandae accusantium?",
+      time: {
+        from: {
+          year: 0,
+        },
+        to: {
+          year: 0,
+        },
+      },
+    },
+  ],
+  skills: [
+    {
+      title: "Html",
+      perc: 100,
+    },
+    {
+      title: "CSS",
+      perc: 100,
+    },
+    {
+      title: "JavaScript",
+      perc: 80,
+    },
+    {
+      title: "React",
+      perc: 70,
+    },
+  ],
+  hobbies: [
+    { title: "Sport/Gym" },
+    { title: "PC Games" },
+    { title: "Table top games/DnD" },
+    { title: "Fantasy books and movies/tv shows" },
+  ],
+};
